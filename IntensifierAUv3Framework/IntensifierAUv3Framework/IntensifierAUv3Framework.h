e3399a7f86d7056f005889289b5396732f5913a0
Evan Murray <evanmurray22@gmail.com>
2021-06-07 12:31:10 -0500

    Add AUv3 Framework

---
+++ IntensifierAUv3Framework/IntensifierAUv3Framework/IntensifierAUv3Framework.h
@@ -0,0 +1,18 @@
+//
+//  IntensifierAUv3Framework.h
+//  IntensifierAUv3Framework
+//
+//  Created by Evan Murray on 6/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IntensifierAUv3Framework.
+FOUNDATION_EXPORT double IntensifierAUv3FrameworkVersionNumber;
+
+//! Project version string for IntensifierAUv3Framework.
+FOUNDATION_EXPORT const unsigned char IntensifierAUv3FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntensifierAUv3Framework/PublicHeader.h>
+
+
