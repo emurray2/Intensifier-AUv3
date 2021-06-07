4c9cf195a6ed3fcb8a29550f8fb069d1f80d7734
Evan Murray <evanmurray22@gmail.com>
2021-06-07 15:03:47 -0500

    Finish BufferedAudioBus class

--- IntensifierAUv3/Shared/AudioUnit/Support/BufferedAudioBus.hpp
+++ IntensifierAUv3/Shared/AudioUnit/Support/BufferedAudioBus.hpp
@@ -1,9 +1,79 @@
 #import <AudioToolbox/AudioToolbox.h>
 #import <AudioUnit/AudioUnit.h>
 #import <AVFoundation/AVFoundation.h>
+#import <algorithm>
 
 struct BufferedAudioBus {
     AUAudioUnitBus* bus = nullptr;
     AUAudioFrameCount maxFrames = 0;
-};
+    AVAudioPCMBuffer* pcmBuffer = nullptr;
+    AudioBufferList const* originalAudioBufferList = nullptr;
+    AudioBufferList* mutableAudioBufferList = nullptr;
 
+    void init(AVAudioFormat* defaultFormat, AVAudioChannelCount maxChannels)
+    {
+        maxFrames = 0;
+        pcmBuffer = nullptr;
+        originalAudioBufferList = nullptr;
+        mutableAudioBufferList = nullptr;
+        bus = [[AUAudioUnitBus alloc] initWithFormat:defaultFormat error:nil];
+        bus.maximumChannelCount = maxChannels;
+    }
+    void allocateRenderResources(AUAudioFrameCount inMaxFrames)
+    {
+        maxFrames = inMaxFrames;
+        pcmBuffer = [[AVAudioPCMBuffer alloc] initWithPCMFormat:bus.format frameCapacity:maxFrames];
+        originalAudioBufferList = pcmBuffer.audioBufferList;
+        mutableAudioBufferList = pcmBuffer.mutableAudioBufferList;
+    }
+    void deallocateRenderResources()
+    {
+        pcmBuffer = nullptr;
+        originalAudioBufferList = nullptr;
+        mutableAudioBufferList = nullptr;
+    }
+};
+struct BufferedOutputBus: BufferedAudioBus {
+    void prepareOutputBufferList(AudioBufferList* outBufferList, AVAudioFrameCount frameCount, bool zeroFill)
+    {
+        UInt32 byteSize = frameCount * sizeof(float);
+        for (UInt32 i = 0; i < outBufferList->mNumberBuffers; ++i)
+        {
+            outBufferList->mBuffers[i].mNumberChannels = originalAudioBufferList->mBuffers[i].mNumberChannels;
+            outBufferList->mBuffers[i].mDataByteSize = byteSize;
+            if (outBufferList->mBuffers[i].mData == nullptr)
+            {
+                outBufferList->mBuffers[i].mData = originalAudioBufferList->mBuffers[i].mData;
+            }
+            if (zeroFill)
+            {
+                memset(outBufferList->mBuffers[i].mData, 0, byteSize);
+            }
+        }
+    }
+};
+struct BufferedInputBus: BufferedAudioBus {
+    AUAudioUnitStatus pullInput(AudioUnitRenderActionFlags* actionFlags,
+                                AudioTimeStamp const* timestamp,
+                                AVAudioFrameCount frameCount,
+                                NSInteger inputBusNumber,
+                                AURenderPullInputBlock pullInputBlock)
+    {
+        if (pullInputBlock == nullptr)
+        {
+            return kAudioUnitErr_NoConnection;
+        }
+        return pullInputBlock(actionFlags, timestamp, frameCount, inputBusNumber, mutableAudioBufferList);
+    }
+    void prepareInputBufferList(UInt32 frameCount)
+    {
+        UInt32 byteSize = std::min(frameCount, maxFrames) * sizeof(float);
+        mutableAudioBufferList->mNumberBuffers = originalAudioBufferList->mNumberBuffers;
+        for (UInt32 i = 0; i < originalAudioBufferList->mNumberBuffers; ++i)
+        {
+            mutableAudioBufferList->mBuffers[i].mNumberChannels = originalAudioBufferList->mBuffers[i].mNumberChannels;
+            mutableAudioBufferList->mBuffers[i].mData = originalAudioBufferList->mBuffers[i].mData;
+            mutableAudioBufferList->mBuffers[i].mDataByteSize = byteSize;
+        }
+    }
+};
