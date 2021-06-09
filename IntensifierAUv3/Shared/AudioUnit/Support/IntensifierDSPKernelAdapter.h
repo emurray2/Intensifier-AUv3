e7c20f5e9ec0440335ceeb79dadd2bd3d3f072ae
Evan Murray <evanmurray22@gmail.com>
2021-06-09 17:18:03 -0500

    Add DSPKernelAdapter declarations

--- IntensifierAUv3/Shared/AudioUnit/Support/IntensifierDSPKernelAdapter.h
+++ IntensifierAUv3/Shared/AudioUnit/Support/IntensifierDSPKernelAdapter.h
@@ -1,1 +1,17 @@
 #import <AudioToolbox/AudioToolbox.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface FilterDSPKernelAdapter : NSObject
+
+@property (nonatomic) AUAudioFrameCount maximumFramesToRender;
+@property (nonatomic, readonly) AUAudioUnitBus *inputBus;
+@property (nonatomic, readonly) AUAudioUnitBus *outputBus;
+
+- (void)setParameter:(AUParameter *)parameter value:(AUValue)value;
+- (AUValue)valueForParameter:(AUParameter *)parameter;
+
+- (void)allocateRenderResources;
+- (void)deallocateRenderResources;
+- (AUInternalRenderBlock)internalRenderBlock;
+
+@end
+NS_ASSUME_NONNULL_END
